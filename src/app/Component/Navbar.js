cb083a5c7a92dbe4caf37f74be34807dc5ecbae0
Sajithmym <sajmymvcool@gmail.com>
2023-07-11 17:08:03 +0530

    change

--- src/app/Component/Navbar.js
+++ src/app/Component/Navbar.js
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
+import './style.css'
 
 
 export default function Navbar() {
   return (
-    <div>
-        <Link href='/'> Home </Link>
-        <Link href='/first'> First </Link>
-        <Link href='/sec'> Second  </Link>
-        <Link href='/third'> Third  </Link>
-    </div>
+    <div> <center>
+      <Link href='/'> Home </Link>
+      <Link href='/first'> First </Link>
+      <Link href='/sec'> Second  </Link>
+      <Link href='/third'> Third  </Link>
+    </center> </div>
   )
 }
